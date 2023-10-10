d201f101059e28a6519008e0d0372782e01cfe67
ericaostini <ericaostini14@gmail.com>
2023-10-10 15:09:30 +0200

    add name, km and age variables and check their insertion

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ const button = document.querySelector(".btn-secondary");
 console.log(button);
 const buttonAnnulla = document.querySelector(".btn-danger");
 console.log(buttonAnnulla);
+
+button.addEventListener("click", function(){
+    const name = document.getElementById("name").value;
+    const km = document.getElementById("km").value;
+    const eta = document.getElementById("eta").value;
+    console.log(km);
+    console.log(eta);
+    console.log(name);
+    if (name === "" || km === "" || eta === ""){
+        alert("inserisci i valori mancanti!");
+    }
+});
