9592f00156e93ae6fae86f3e6c709416e35c4bdf
ericaostini <ericaostini14@gmail.com>
2023-10-10 15:39:56 +0200

    Add offerta biglietto in html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 const button = document.querySelector(".btn-secondary");
 console.log(button);
 const buttonAnnulla = document.querySelector(".btn-danger");
@@ -10,15 +12,17 @@ button.addEventListener("click", function(){
     console.log(km);
     console.log(eta);
     console.log(name);
-    if (name === "" || km === "" || eta === ""){
+    if (name === "" || isNaN(km) || eta === ""){
         alert("inserisci i valori mancanti!");
     } else{
-        const biglietto = document.getElementById("biglietto");
-        const passeggero = biglietto.querySelector("div");
-        passeggero.innerHTML = `
-            <h4>Nome passeggero</h4>
-            <p> ${name} </p>
-        `;
+        const nominativo = document.getElementById("nominativo").innerHTML = `
+        <h4>Nome passeggero</h4>
+        <p> ${name} </p>
+    `;
+        const offerta = document.getElementById("offerta").innerHTML = `
+        <h6>Offerta</h6>
+        <p> ${eta}</p>
+    `;
     }
 });
 
